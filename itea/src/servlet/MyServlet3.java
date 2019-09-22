c91cf74dc3609de26df502f7992c63b8bd659874
Aleksandr <Aleksandr@Akselor>
2019-09-22 19:55:29 +0300

    Remove autogenerated todos

--- itea/src/servlet/MyServlet3.java
+++ itea/src/servlet/MyServlet3.java
@@ -11,8 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 public class MyServlet3 extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//super.doGet(req, resp);
 		PrintWriter out = resp.getWriter();
 		out.print("<a href='myservlet1'>first</a> <a href='myservlet2'>second</a> <a style='font-weight:bold' href='myservlet3'>third</a> <br />Hello from servlet!");
 	}
