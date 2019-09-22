9cd7e90710fc5a40b2ac7fd2f3b996cf5dbcb5e0
Aleksandr <Aleksandr@Akselor>
2019-09-22 19:55:57 +0300

    Really remove autogenerated todos

--- itea/src/servlet/MyServlet.java
+++ itea/src/servlet/MyServlet.java
@@ -21,14 +21,12 @@ public class MyServlet extends HttpServlet {
      */
     public MyServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		PrintWriter out = response.getWriter();
 		out.write("Hello from Server");
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
@@ -38,7 +36,6 @@ public class MyServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
