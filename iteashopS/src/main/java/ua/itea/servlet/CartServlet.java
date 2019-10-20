482efb566e534d0d2c2661d2c0c95f39745501d0
Oleksandr Naumov <oleksandr.naumov91@gmail.com>
2019-10-20 20:01:04 +0300

    Cleanup CartServlet

--- iteashopS/src/main/java/ua/itea/servlet/CartServlet.java
+++ iteashopS/src/main/java/ua/itea/servlet/CartServlet.java
@@ -11,7 +11,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,28 +26,21 @@ import ua.itea.models.Product;
 @Controller
 @RequestMapping("/cart")
 public class CartServlet {
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
 	protected String doGet(HttpSession session, ModelMap mapping) {
 		Cart cart = (Cart) session.getAttribute("cart");
-		List<Product> productList = Collections.emptyList();
 		Map<Product,Integer> productMap = null;
 		if (cart != null) {
-			productList = cart.getProductList();
 			productMap = cart.getProductMap();
 		}
-		mapping.addAttribute("productList", productList);
 		mapping.addAttribute("productMap", productMap);
-		mapping.addAttribute("returnLink", "./cart");
 		return "CartView";
 	}
 
-	@RequestMapping(method = RequestMethod.POST, params = { "productToBuy"})
+	@PostMapping(params = { "productToBuy"})
 	@ResponseBody
 	protected String addProduct(HttpSession session, long productToBuy) {
-		System.out.println("JQ request");
-		DAOFactory factory = DAOFactory.getDAOFactory(1);
-		ProductDAO productDAO = factory.getProductDAO();
-		Product product = productDAO.getProductById(productToBuy);
+		Product product = getProduct(productToBuy);
 		if (product != null) {
 			Cart cart = (Cart) session.getAttribute("cart");
 			if (cart == null) cart = new Cart();
@@ -55,12 +50,10 @@ public class CartServlet {
 		}
 		return "Error";
 	} 
+	
 	@DeleteMapping("/{productToRem}")
 	protected ResponseEntity<?> doDelete(HttpSession session, @PathVariable Long productToRem) {
-		System.out.println("Delete Product by DELETE method" + productToRem);
-		DAOFactory factory = DAOFactory.getDAOFactory(1);
-		ProductDAO productDAO = factory.getProductDAO();
-		Product product = productDAO.getProductById(productToRem);
+		Product product = getProduct(productToRem);
 		HttpStatus httpStatus = HttpStatus.NOT_MODIFIED;
 		String response ="Error";
 		if (product != null) {
@@ -75,5 +68,12 @@ public class CartServlet {
 			}
 		}
 		return new ResponseEntity<String>(response, httpStatus);
+	}
+	
+	private Product getProduct(long id) {
+		DAOFactory factory = DAOFactory.getDAOFactory(1);
+		ProductDAO productDAO = factory.getProductDAO();
+		Product product = productDAO.getProductById(id);
+		return product;
 	}
 }
