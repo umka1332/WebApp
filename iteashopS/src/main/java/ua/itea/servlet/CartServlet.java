998e18f9e862f02ee942543129fda68365688834
Oleksandr Naumov <oleksandr.naumov91@gmail.com>
2019-10-20 20:09:20 +0300

    More cleanup

--- iteashopS/src/main/java/ua/itea/servlet/CartServlet.java
+++ iteashopS/src/main/java/ua/itea/servlet/CartServlet.java
@@ -1,13 +1,10 @@
 package ua.itea.servlet;
 
-import java.util.Collections;
-import java.util.List;
+
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import ua.itea.dao.DAOFactory;
@@ -30,9 +26,8 @@ public class CartServlet {
 	protected String doGet(HttpSession session, ModelMap mapping) {
 		Cart cart = (Cart) session.getAttribute("cart");
 		Map<Product,Integer> productMap = null;
-		if (cart != null) {
+		if (cart != null)
 			productMap = cart.getProductMap();
-		}
 		mapping.addAttribute("productMap", productMap);
 		return "CartView";
 	}
@@ -52,22 +47,18 @@ public class CartServlet {
 	} 
 	
 	@DeleteMapping("/{productToRem}")
-	protected ResponseEntity<?> doDelete(HttpSession session, @PathVariable Long productToRem) {
+	@ResponseBody
+	protected String doDelete(HttpSession session, @PathVariable Long productToRem) {
 		Product product = getProduct(productToRem);
-		HttpStatus httpStatus = HttpStatus.NOT_MODIFIED;
-		String response ="Error";
 		if (product != null) {
 			Cart cart = (Cart) session.getAttribute("cart");
 			if (cart != null) {
 				boolean res = cart.subProduct(product);
 				session.setAttribute("cart", cart);
-				if (res) {
-					httpStatus = HttpStatus.OK;
-					response = "Ok";
-				}
+				if (res) return "Ok";
 			}
 		}
-		return new ResponseEntity<String>(response, httpStatus);
+		return "Error";
 	}
 	
 	private Product getProduct(long id) {
--- iteashopS/src/main/java/ua/itea/servlet/ProductServlet.java
+++ iteashopS/src/main/java/ua/itea/servlet/ProductServlet.java
@@ -2,8 +2,8 @@ package ua.itea.servlet;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import ua.itea.dao.DAOFactory;
 import ua.itea.dao.ProductDAO;
@@ -11,18 +11,16 @@ import ua.itea.dao.ProductDAO;
 @Controller
 @RequestMapping("/products")
 public class ProductServlet {
-	@RequestMapping(method = RequestMethod.GET, params = "category")
+	@GetMapping(params = "category")
 	protected String doGet(long category, ModelMap mapping) {
 		ProductDAO productDAO = DAOFactory.getDAOFactory(1).getProductDAO();
-		mapping.addAttribute("returnLink", "./products?category=" + category);
 		mapping.addAttribute("productList", productDAO.getProductListByCategory(category));
 		return "ProductsView";
 	}
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
 	protected String doGet(ModelMap mapping) {
 		ProductDAO productDAO = DAOFactory.getDAOFactory(1).getProductDAO();
-		mapping.addAttribute("returnLink", "./products");
 		mapping.addAttribute("productList", productDAO.getProductList());
 		return "ProductsView";
 	}
